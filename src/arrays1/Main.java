7fa87f53208d4607461e324d4cc9a155f39b77b2
AlexeyBuzmakov <Alexey_Buzmakov@mail.ru>
2022-03-29 20:46:17 +0300

    Update

--- src/arrays1/Main.java
+++ src/arrays1/Main.java
@@ -3,8 +3,6 @@ package arrays1;
 public class Main {
     public static void main(String[] args) {
         Array array = new Array();
-        String[][]arr = {{"Dog", "Cat", "Monkey"},{"Lion", "Snake", "Bird"}};
-        String[][]arr2 = {{"Dog", "Cat", "Monkey"},{"Lion", "Snake", "Bird"}};
-        System.out.println(array.checkEqualityDipArrays(arr, arr2));
+        System.out.println(array.checkEqualityDipArrays());
     }
 }
