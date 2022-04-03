15a09f2733c0ced42f7defb13905d80c408ca9f4
AlexeyBuzmakov <Alexey_Buzmakov@mail.ru>
2022-04-04 02:00:42 +0300

    Update

--- src/exeption/exeption1/Main.java
+++ src/exeption/exeption1/Main.java
@@ -5,13 +5,14 @@ public class Main {
 
         int result = 0;
         try{
-           result = Calculator.divisionNumbers(20,0, '/');
+           result = Calculator.divisionNumbers(20,4, '/');
+            System.out.println(result);
         }
         catch (ArithmeticException exception) {
             System.out.println(exception.getMessage());
         }
         finally {
-            System.out.println(result);
+            System.out.println("Помни что на 0 делить нельзя");
         }
     }
 }
