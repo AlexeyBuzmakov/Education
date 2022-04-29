7460228a8e412cf2f4c701071b44727031997bda
AlexeyBuzmakov <Alexey_Buzmakov@mail.ru>
2022-04-30 00:43:19 +0300

    Update

--- src/collections/list/Main.java
+++ src/collections/list/Main.java
@@ -4,31 +4,29 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        UtilsList utils = new UtilsList();
-        List<Number>arrayList = new ArrayList<>();
-        List<Number>linkedList = new LinkedList<>();
-        Integer[]arr = {5, 16, 93, 54, 111, 8, 37};
+        List<Number>arrayList = new ArrayList<>(Arrays.asList(19, 19, 22, 15, 47, 47 , 47, 13 ,8 , 94, 111, 111 , 3));
+        List<Number>linkedList = new LinkedList<>(Arrays.asList(44, 12 , 88, 88, 88, 34, 92, 3, 55, 55, 21, 100,156));
+        Integer[]arr = {5, 16, 93, 54, 111, 8, 37, 44, 12 ,37, 91, 4, 137};
 
-        utils.fillLists(arrayList, linkedList);
+//        System.out.println(UtilsList.sum(arrayList));
+//        System.out.println(UtilsList.sumEven(arrayList));
+//        System.out.println(UtilsList.countRepet(arrayList, 5));
+//        System.out.println(UtilsList.max(arrayList));
+//        System.out.println(UtilsList.min(arrayList));
+//        UtilsList.removeRepet(arrayList);
+//        System.out.println(Arrays.toString(UtilsList.conversionToArray(arrayList)));
+//        System.out.println(UtilsList.conversionToList(arr));
+//        UtilsList.removeEveryThird(linkedList);
+//        UtilsList.removeFirstLast(linkedList);
+//        UtilsList.insertMiddle(arrayList, arr);
+//        UtilsList.removeIdenticalRow(arrayList);
+//        UtilsList.increaseCapacity((ArrayList<Number>) arrayList);
+//        UtilsList.merger(arrayList, linkedList);
 
-//        System.out.println(utils.sum(arrayList));
-//        System.out.println(utils.sumEven(arrayList));
-//        System.out.println(utils.countRepet(arrayList, 5));
-//        System.out.println(utils.max(arrayList));
-//        System.out.println(utils.min(arrayList));
-//        System.out.println(utils.removeRepet(arrayList));
-//        System.out.println(Arrays.toString(utils.conversionToArray(arrayList)));
-//        System.out.println(utils.conversionToList(arr));
-//        System.out.println(utils.removeEveryThird(linkedList));
-//        System.out.println(utils.removeFirstLast(linkedList));
-//        System.out.println(utils.insertMiddle(arrayList, arr));
-//        System.out.println(utils.removeIdenticalRow(arrayList));
-//        utils.increaseCapacity((ArrayList<Number>) arrayList);
-//        utils.merger(arrayList, linkedList);
-//        System.out.println(UtilsList.of(arrayList, linkedList));
-//
+//        List<Number>list = new ArrayList<>(new ArrayList<>(UtilsList.of(arrayList, linkedList)));
+
 //        Number[]array = new Number[100];
-//        System.out.println(Arrays.toString(utils.fillArray(array)));
-//        System.out.println(Arrays.toString(utils.trimToSize(array)));
+//        UtilsList.fillArray(array);
+//        System.out.println(Arrays.toString(UtilsList.trimToSize(array)));
     }
 }
--- src/collections/list/UtilsList.java
+++ src/collections/list/UtilsList.java
@@ -4,18 +4,7 @@ import java.util.*;
 
 public class UtilsList {
 
-    public void fillLists(List<Number> arrayList, List<Number> linkedList) {
-        for (int i = 0; i < 10; i++) {
-            Random random = new Random();
-            int num = random.nextInt(100);
-            arrayList.add(num);
-            linkedList.add(num);
-        }
-        System.out.println(arrayList);
-        System.out.println(linkedList);
-    }
-
-    public double sum(List<Number> list) {                       //1.1
+    public static double sum(List<Number> list) {                       //1.1
         double sum = 0;
         for(Number num : list) {
             sum += num.doubleValue();
@@ -23,7 +12,7 @@ public class UtilsList {
         return sum;
     }
 
-    public double sumEven(List<Number>list) {                    //1.2
+    public static double sumEven(List<Number>list) {                    //1.2
         double sum = 0;
         for(int i = 1; i < list.size(); i++) {
             sum += list.get(i).doubleValue();
@@ -32,7 +21,7 @@ public class UtilsList {
         return sum;
     }
 
-    public int countRepet(List<Number>list, Number value) {       //1.3
+    public static int countRepet(List<Number>list, Number value) {       //1.3
         int count = 0;
         for(Number element : list) {
             if(element.equals(value)) {
@@ -42,7 +31,7 @@ public class UtilsList {
         return count;
     }
 
-    public double max(List<Number>list) {                         //1.4
+    public static double max(List<Number>list) {                         //1.4
         double max = Double.MIN_VALUE;
         for(Number num : list) {
             if(num.doubleValue() > max) {
@@ -52,7 +41,7 @@ public class UtilsList {
         return max;
     }
 
-    public double min(List<Number>list) {                         //1.5
+    public static double min(List<Number>list) {                         //1.5
         double min = Double.MAX_VALUE;
         for(Number num : list) {
             if(num.doubleValue() < min) {
@@ -62,18 +51,18 @@ public class UtilsList {
         return min;
     }
 
-    public List<Number> removeRepet(List<Number>list) {            //2
+    public static void removeRepet(List<Number>list) {            //2
         for(int i = 0; i < list.size(); i++) {
             for(int j = i + 1; j < list.size(); j++) {
                 if(list.get(i).equals(list.get(j))) {
                     list.remove(j);
+                    j--;
                 }
             }
         }
-        return list;
     }
 
-    public Number[] conversionToArray(List<Number>list) {          //3
+    public static Number[] conversionToArray(List<Number>list) {          //3
         Number[]arr = new Number[list.size()];
         for(int i = 0; i < arr.length; i++) {
             arr[i] = list.get(i);
@@ -81,87 +70,72 @@ public class UtilsList {
         return arr;
     }
 
-    public List<Number> conversionToList(Number[]arr) {            //4
+    public static List<Number> conversionToList(Number[]arr) {            //4
         return new ArrayList<>(Arrays.asList(arr));
     }
 
-    public List<Number> removeEveryThird(List<Number>list) {       //5
-        for(int i = 2; i < list.size(); i++) {
+    public static void removeEveryThird(List<Number>list) {       //5
+        for(int i = 2; i < list.size(); i += 2) {
             list.remove(i);
-            i = i + 1;
         }
-        return list;
     }
 
-    public List<Number> removeFirstLast(List<Number>list) {        //6
+    public static void removeFirstLast(List<Number>list) {        //6
         list.remove(0);
         list.remove(list.size() - 1);
-        return list;
     }
 
-    public List<Number> insertMiddle(List<Number> list, Number[] arr) {         //7
-        int l = list.size() / 2;                                               // тут должно быть решение получше
-        for(int i = 0; i < arr.length; i++) {
-            list.add(null);
+    public static void insertMiddle(List<Number> list, Number[] arr) {         //7
+        int middle = list.size() / 2;
+        for (Number number : arr) {
+            list.add(middle, number);
+            middle++;
         }
-        int j = 0;
-        for (int i = l; i < l + arr.length; i++) {
-            if(j < l) {
-                list.set(list.size() - l + j, list.get(i));
-            }
-            list.set(i, arr[j]);
-            j++;
-        }
-        return list;
+        System.out.println(list);
     }
 
-    public List<Number> removeIdenticalRow(List<Number>list) {     //8
+    public static void removeIdenticalRow(List<Number>list) {     //8
         for(int i = 0; i < list.size() - 1; i++) {
             if(list.get(i).equals(list.get(i + 1))) {
                list.remove(i + 1);
+               i--;
             }
         }
-        return list;
     }
 
-    public void increaseCapacity(ArrayList<Number>list) {          //9
+    public static void increaseCapacity(ArrayList<Number>list) {          //9
         list.ensureCapacity(1_000_000);
     }
 
-    public void merger(List<Number>arrayList, List<Number>linkedList) {     //10
+    public static void merger(List<Number>arrayList, List<Number>linkedList) {     //10
         int l = arrayList.size();
         arrayList.addAll(linkedList);
         for(int i = 0; i < l; i++) {
             linkedList.add(arrayList.get(i));
         }
-        System.out.println(arrayList);
-        System.out.println(linkedList);
     }
 
     @SafeVarargs
-    public static <E> List<E> of(E...elements) {                   //11    тут не понял как реализовывается
+    public static <E> List<E> of(E...elements) {                            //11
         return List.of(elements);
     }
 
-    public Number[] fillArray(Number[]arr) {                            //12
+    public static void fillArray(Number[]arr) {                            //12
         Random random = new Random();
         for(int i = 0; i < 20; i++) {
            arr[i] = random.nextInt(100);
         }
-        return arr;
     }
 
-    public Number[] trimToSize(Number[] arr) {                             //12
+    public static Number[] trimToSize(Number[] arr) {
         int count = 0;
-        for (Number num : arr) {
-            if (num != null) {
-                count++;
-            }
+        int i = arr.length - 1;
+        while(arr[i] == null) {
+           i--;
+           count++;
         }
-        Number[] array = new Number[count];
-        for (int i = 0; i < count; i++) {
-            array[i] = arr[i];
-        }
+        Number[] array = new Number[arr.length - count];
+        System.arraycopy(arr, 0, array, 0, array.length);
         return array;
     }
 }
