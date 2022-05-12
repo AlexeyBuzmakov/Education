1fb2411aea61461490036a3100737532855d94fb
AlexeyBuzmakov <Alexey_Buzmakov@mail.ru>
2022-05-13 00:35:53 +0300

    Update

--- src/collections/map/task/repetnumbers/Main.java
+++ src/collections/map/task/repetnumbers/Main.java
@@ -1,8 +1,20 @@
 package collections.map.task.repetnumbers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         int[]arr = {5, 16, 5, 97, 3, 5, 97, 11, 88, 5, 3};
-        Utils.printMap(arr);
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(Integer numb : arr) {
+            if(map.containsKey(numb)) {
+                map.put(numb, map.get(numb) + 1);
+                continue;
+            }
+            map.put(numb, 0);
+        }
+        System.out.println(map);
     }
 }
--- src/collections/map/task/sortkeys/Main.java
+++ src/collections/map/task/sortkeys/Main.java
@@ -1,29 +1,25 @@
 package collections.map.task.sortkeys;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
-        map.put(5, "Pushkin");
-        map.put(7, "Tytchev");
-        map.put(3, "Dostoevskiy");
+        map.put(37, "Pushkin");
+        map.put(16, "Tytchev");
+        map.put(22, "Dostoevskiy");
         map.put(8, "Tolstoy");
-        map.put(1, "Lermontov");
+        map.put(81, "Lermontov");
         map.put(9, "Gorkiy");
-        map.put(2, "Nekrasov");
+        map.put(33, "Nekrasov");
         map.put(4, "Gogol");
-        map.put(6, "Kuprin");
+        map.put(64, "Kuprin");
 
-        Map<Integer, String> sortedMap = new TreeMap<>(map);
-        map.putAll(sortedMap);
-
-        Set<Map.Entry<Integer, String>> entries = map.entrySet();
-        for(Map.Entry<Integer, String> entry : entries) {
-            System.out.println(entry.getKey() + "-" + entry.getValue());
+        List<Integer> list = new ArrayList<>(map.keySet());
+        
+        Collections.sort(list);
+        for(Integer lists : list) {
+            System.out.println(lists + "-" + map.put(lists, map.get(lists)));
         }
     }
 }
