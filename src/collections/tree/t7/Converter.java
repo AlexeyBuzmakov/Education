bec5515c62e53050573313d47c19af4c6375dd42
AlexeyBuzmakov <Alexey_Buzmakov@mail.ru>
2022-05-26 12:21:22 +0300

    Update

--- src/collections/tree/t7/Converter.java
+++ src/collections/tree/t7/Converter.java
@@ -17,35 +17,25 @@ public class Converter {
     }
 
     public StringBuilder convert(int num) {
-        if(num <= 0) {
-            return new StringBuilder("Некорректный ввод");
+        if (num <= 0) {
+            return new StringBuilder("Для конвертации подходят только положительные числа");
         }
         createConverter();
         StringBuilder str = new StringBuilder();
         while (num != 0) {
-            if (num < 1000 && num > 100) {
-                if (numb.higherKey(num) - num <= 100) {
-                    str.append(numb.get(100)).append(numb.get(numb.ceilingKey(num)));
-                    num -= numb.ceilingKey(num) - 100;
-                    continue;
-                }
+            if (num < 1000 && num > 100 && numb.higherKey(num) - num <= 100) {
+                str.append(numb.get(100)).append(numb.get(numb.ceilingKey(num)));
+                num -= numb.ceilingKey(num) - 100;
+            } else if (num < 100 && num > 10 && numb.higherKey(num) - num <= 10) {
+                str.append(numb.get(10)).append(numb.get(numb.ceilingKey(num)));
+                num -= numb.ceilingKey(num) - 10;
+            } else if (num < 10 && numb.higherKey(num) - num == 1) {
+                str.append(numb.get(1)).append(numb.get(numb.ceilingKey(num)));
+                num -= numb.ceilingKey(num) - 1;
+            } else {
+                str.append(numb.get(numb.floorKey(num)));
+                num -= numb.floorKey(num);
             }
-            if (num < 100 && num > 10) {
-                if (numb.higherKey(num) - num <= 10) {
-                    str.append(numb.get(10)).append(numb.get(numb.ceilingKey(num)));
-                    num -= numb.ceilingKey(num) - 10;
-                    continue;
-                }
-            }
-            if (num < 10) {
-                if (numb.higherKey(num) - num == 1) {
-                    str.append(numb.get(1)).append(numb.get(numb.ceilingKey(num)));
-                    num -= numb.ceilingKey(num) - 1;
-                    continue;
-                }
-            }
-            str.append(numb.get(numb.floorKey(num)));
-            num -= numb.floorKey(num);
         }
         return str;
     }
