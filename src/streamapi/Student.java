526207688fc41deedb8917f479e4c20313698d58
AlexeyBuzmakov <Alexey_Buzmakov@mail.ru>
2022-06-27 04:54:53 +0400

    Update

---
+++ src/streamapi/Student.java
@@ -0,0 +1,27 @@
+package streamapi;
+
+import java.util.List;
+import java.util.Map;
+
+public class Student {
+    private String name;
+    private Map<String, List<Integer>>marks;
+
+    public Student(String name, Map<String, List<Integer>>marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, List<Integer>> getMarks() {
+        return marks;
+    }
+
+    @Override
+    public String toString() {
+        return name + marks;
+    }
+}
